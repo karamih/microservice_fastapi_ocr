7d88421f579142a1278d196bd110079fafd38a39
hossein <karamihossein.ai@gmail.com>
2023-10-23 03:34:16 +0330

    get file images and test

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 import pathlib
+import io
+import uuid
 
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, UploadFile, HTTPException, status
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from PIL import Image
 
 from src.settings import settings
 
-
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploads"
+VALID_MEDIA_EXTENSIONS = ["jpg", "jpeg", "png"]
 
 app = FastAPI(debug=settings.debug)
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
@@ -21,3 +25,23 @@ def index(request: Request):
 @app.post("/")
 def index():
     return {"messages": "Done"}
+
+
+@app.post("/upload", response_class=FileResponse, status_code=status.HTTP_201_CREATED)
+async def upload_img(file: UploadFile):
+    file_type = pathlib.Path(file.filename).suffix.replace(".", "")
+    if file_type not in VALID_MEDIA_EXTENSIONS:
+        raise HTTPException(
+            detail="Use jpg, jpeg or png.", status_code=status.HTTP_400_BAD_REQUEST
+        )
+    UPLOAD_DIR.mkdir(exist_ok=True)
+    content_stream = io.BytesIO(await file.read())
+    try:
+        image = Image.open(content_stream)
+    except:
+        raise HTTPException(
+            detail="Can not open Image!", status_code=status.HTTP_400_BAD_REQUEST
+        )
+    full_path = UPLOAD_DIR / f"{uuid.uuid4()}.{file_type}"
+    image.save(full_path)
+    return full_path
--- src/test_endpoints.py
+++ src/test_endpoints.py
@@ -1,5 +1,10 @@
+import time
+import shutil
+
 from fastapi.testclient import TestClient
-from src.main import app
+from PIL import Image
+
+from src.main import app, BASE_DIR, UPLOAD_DIR, VALID_MEDIA_EXTENSIONS
 
 
 client = TestClient(app)
@@ -15,3 +20,25 @@ def test_post_index():
     response = client.post(url="/")
     assert response.status_code == 200
     assert "application/json" in response.headers["content-type"]
+
+
+def test_post_img():
+    test_images = BASE_DIR / "images"
+    for img in test_images.glob("*"):
+        response = client.post(url="/upload", files={"file": open(img, "rb")})
+        try:
+            image = Image.open(img)
+        except:
+            image = None
+
+        if image is None:
+            assert response.status_code == 400, "image cant read."
+        else:
+            assert (
+                response.headers["content-type"].split("/")[-1]
+                in VALID_MEDIA_EXTENSIONS
+            ), "check valid extension"
+            assert response.status_code == 201, "everything successfully"
+
+    time.sleep(2)
+    shutil.rmtree(UPLOAD_DIR)
